37be6a60bc5b96c0da0b793da97a1f0450467f11
NIZEYIMANA Jean de Dieu <86121130+Jadoiconic@users.noreply.github.com>
2024-01-20 22:42:57 +0200

    mongosanitize and xss

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet")
+const mongoSanitize = require("express-mongo-sanitize")
+const xss = require("xss-clean")
+
 const UsersRouter = require("./routes/userRoutes");
 const TourRouter = require("./routes/toursRoutes");
 const AppError = require("./utils/appError");
-const globalErrorHandler = require("./controllers/errorController")
+const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
-// Middlewares
+// Global Middlewares
+// Set security HTTP headers
+app.use(helmet())
 
 process.env.NODE_ENV === "develoment" && app.use(morgan("dev"));
-app.use(express.json());
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message:"Too many requests from this IP, Please try again in an hour"
+});
+ 
+app.use('/api',limiter)
+
+// Body parser, reading data from body into  req.body
+app.use(express.json({limit:'10kb'}));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize())
+
+// Data sanitization against XSS
+app.use(xss())
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
@@ -26,6 +51,6 @@ app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use(globalErrorHandler); 
+app.use(globalErrorHandler);
 
 module.exports = app;
